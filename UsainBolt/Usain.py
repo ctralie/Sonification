f752b6dfb6d92816fcacd8b129daddf02fbd14c9
mtecce <60228508+mtecce@users.noreply.github.com>
2020-07-09 12:36:45 -0400

    added tests to Usainbolt

--- UsainBolt/Usain.py
+++ UsainBolt/Usain.py
@@ -3,10 +3,31 @@ import matplotlib.pyplot as plt
 
 def create_chords(freqscale, freq, arr, acceleration, arrl, typeofmod=0):
     
+    """
+    Creates an audio array comprised of major and minor 7th chords from a time-series array
+    Parameters
+    ----------
+    freqscale:   int
+        value to scale the frequency range by
+    freq:    int
+        base frequency for start of array chord
+    arr:    array()
+        time-series to be sonified
+    acceleration:   array()
+        a derivative(or 2nd derivative) of arr
+        determines if chord will be major or minor
+    arrl:   int
+        last value of np.linspace array which holds the time series
+    typeofmod:   int
+        indicator for what type of array was sent in for sonification
+        0 = position array, 1 = velocity array
+        
+    Return
+    ------
+    CHORD:   array()
+        array comprised of 4 distinct sin waves from the arr param
     
-    ##typeofmod: position arr = 0 velocity arr = 1
-    
-    
+    """
     fs = 44100
     
     #setting 1st and 3rd harmonies
@@ -26,20 +47,19 @@ def create_chords(freqscale, freq, arr, acceleration, arrl, typeofmod=0):
             #do minor 7th
             NT[i] = arr[i] * freq * (2**(3/12))
             NF[i] = arr[i] * freq * (2**(10/12))
-
-    #making sin waves
+    
+    #chose scale type
     if typeofmod == 0:
-        AO = np.sin(2*np.pi*NO/freqscale)
-        AT = np.sin(2*np.pi*NT/freqscale)
-        ATH = np.sin(2*np.pi*NTH/freqscale)
-        AF = np.sin(2*np.pi*NF/freqscale)
-        #return chord
-        return AO + AT + ATH + AF
-    else:
-        AO = np.sin(2*np.pi*NO*freqscale)
-        AT = np.sin(2*np.pi*NT*freqscale)
-        ATH = np.sin(2*np.pi*NTH*freqscale)
-        AF = np.sin(2*np.pi*NF*freqscale)
-        #return chord
-        return AO + AT + ATH + AF
+        freqscale = 1/freqscale
+    
+    #making sin waves
+    AO = np.sin(2*np.pi*NO*freqscale)
+    AT = np.sin(2*np.pi*NT*freqscale)
+    ATH = np.sin(2*np.pi*NTH*freqscale)
+    AF = np.sin(2*np.pi*NF*freqscale)
+    
+    #return chord
+    CHORD =  AO + AT + ATH + AF
+    return CHORD
+
         
