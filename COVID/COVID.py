f4551ca3c47e82fd35520f4792909c11d69c1105
mtecce <60228508+mtecce@users.noreply.github.com>
2020-07-15 09:20:15 -0400

    COVID file, almost finished

--- COVID/COVID.py
+++ COVID/COVID.py
@@ -9,10 +9,13 @@ Created on Fri Jul 10 13:37:50 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import IPython.display as ipd
 from scipy import interpolate
 import sys
 sys.path.append("..")
+sys.path.append('../Viterbi')
 from SlidingWindow import *
+from Viterbi import getCSM
 
 def get_data(countrycode):
 
@@ -62,7 +65,7 @@ def interp_data(Data, DAF):
     fac = len(DAF)/len(Data)
     N = len(Data)
     x = np.linspace(0, 1, N)
-    f = interpolate.interp1d(x,Data,kind='cubic')
+    f = interpolate.interp1d(x,Data,kind='linear')
     xnew = np.linspace(0, 1, int(fac*N))
     return f(xnew)
 
@@ -72,34 +75,146 @@ def create_2D_shape(Cases,Deaths):
     Shape[:,0] = Cases[:]
     Shape[:,1] = Deaths[:]
     return Shape
+ 
+def get_country_max(DO,DT):
+    domax = np.max(DO)
+    dtmax = np.max(DT)
+    return domax,dtmax
+    
+def create_note_grid_arrays(cmax,dmax):
+    """
+    MAJ7:
+        a,e
+    MAJ:
+        a,f
+        b,e
+    MIN:
+        a,g
+        b,f
+        c,e
+    MIN7:
+        a,h
+        b,g
+        c,f
+        d,e
+    FDIM:
+        b,h
+        c,g
+        d,f
+    DIM:
+        c,h
+        d,g
+    TRI:
+       d,h
+    """
+    a = 0
+    b =  cmax/3
+    c = 2*cmax/3
+    d =  cmax
+    e = 0
+    f = dmax/3
+    g = 2*dmax/3
+    h = dmax
+    MAJS = np.array([[a, e]])
+    MAJ = np.array([[a,f],[b,e]])
+    MIN = np.array([[a,g],[b,f],[c,e]])
+    MINS = np.array([[a,h],[b,g],[c,f],[d,e]])
+    FDIM = np.array([[b,h],[c,g],[d,f]])
+    DIM = np.array([[c,h],[d,g]])
+    TRI = np.array([[d,h]])
+    return MAJS,MAJ,MIN,MINS,FDIM,DIM,TRI
 
-#covid data from january 20th to july 10th 2020
-UScode = "US"
-ITcode = "IT"
-USData = get_data(UScode)
-ITData = get_data(ITcode)
+def make_chord_arrays(Seconds,Types):
+    
+    fs = 44100
+    note = np.linspace(0,Seconds,fs*Seconds)
+    numchords = int(len(Types))
 
-USC,USD = get_info(USData)
-ITC,ITD = get_info(ITData)
+    one = note * 440 * (2**(0/12))
+    majthree = note * 440 * (2**(5/12))
+    minthree = note * 440 * (2**(4/12))
+    majfive = note * 440 * (2**(7/12))
+    minfive = note * 440 * (2**(6/12))
+    dimfive = note * 440 * (2**(5/12))
+    minseven = note * 440 * (2**(10/12))
+    majseven = note * 440 * (2**(11/12))
+    dimseven = note * 440 * (2**(9/12))
+    disonance = note * 440 * (2**(3/12))
+    one = np.sin(2*np.pi*one)
+    majthree = np.sin(2*np.pi*majthree)
+    minthree = np.sin(2*np.pi*minthree)
+    majfive = np.sin(2*np.pi*majfive)
+    minfive = np.sin(2*np.pi*minfive)
+    dimfive = np.sin(2*np.pi*dimfive)
+    minseven = np.sin(2*np.pi*minseven)
+    majseven = np.sin(2*np.pi*majseven)
+    dimseven = np.sin(2*np.pi*dimseven)
+    disonance = np.sin(2*np.pi*disonance)
+    
+    Chords = np.zeros((len(note),numchords))
 
-fs = 44100
-seconds = 10
-DesiredAudioFrame = np.linspace(0, 1, int(fs*seconds))
+    for i in range(numchords):
+        if Types[i] == "Major7":
+            Chords[:,i] = one + majthree + majfive + majseven
+        elif Types[i] == "Major":
+            Chords[:,i] = one + majthree + majfive
+        elif Types[i] == "Minor": 
+            Chords[:,i] = one + minthree + majfive
+        elif Types[i] == "Minor7":
+            Chords[:,i] = one + minthree + majfive + minseven
+        elif Types[i] == "FullyDim":
+            Chords[:,i] = one + minthree + dimfive + dimseven
+        elif Types[i] == "Dim":
+            Chords[:,i] = one + minthree + dimfive
+        elif Types[i] == "Tritone":
+            Chords[:,i] = one + disonance + minfive + dimseven
+    return Chords
 
-IUSC,IUSD,IITC,IITD = get_interp(USC,USD,ITC,ITD,DesiredAudioFrame)
+def amp_mod_audio(dists,AS,modamp):
+    
+    Audio = AS
+    x = 0
+    
+    for i in range(len(dists[x])):
+        dfac = len(AS)/len(dists)
+        dN = len(dists)
+        dx = np.linspace(0, 1, dN)
+        fd = interpolate.interp1d(dx,dists[:,i],kind='cubic')
+        dxnew = np.linspace(0, 1, int(dfac*dN))
+        D = fd(dxnew)
+        
+        #apply amplitude modulation (I used exponential because simple multiplication didn't have a usable effect)
+        ampchange = 1.0 + modamp
+        Audio += AS[:] / (D[:]**ampchange)
+        x += 1
+        
+    return Audio
 
-USShape = create_2D_shape(IUSC,IUSD)
-ITShape = create_2D_shape(IITC,IITD)
-
-plt.scatter(USShape[:, 0], USShape[:, 1], c = DesiredAudioFrame)
-plt.xlabel("New Cases Per Day")
-plt.ylabel("New Deaths Per Day")
-plt.title("United States")
-plt.show()
-plt.figure()
-plt.scatter(ITShape[:, 0], ITShape[:, 1], c = DesiredAudioFrame)
-plt.xlabel("New Cases Per Day")
-plt.ylabel("New Deaths Per Day")
-plt.title("Italy")
-plt.show()
-
+def create_audio(Data,MAJS,MAJ,MIN,MINS,FDIM,DIM,TRI,Chords):
+    
+    modamp = .25
+    
+    MAJS = getCSM(Data, MAJS)
+    AMJS = amp_mod_audio(MAJS,Chords[:,0],modamp)
+    
+    MAJ = getCSM(Data, MAJ)
+    AMJ = amp_mod_audio(MAJ,Chords[:,1],modamp)
+    
+    MIN = getCSM(Data, MIN)
+    AMN = amp_mod_audio(MIN,Chords[:,2],modamp)
+    
+    MINS = getCSM(Data, MINS)
+    AMNS = amp_mod_audio(MINS,Chords[:,3],modamp)
+    
+    FDIM = getCSM(Data, FDIM)
+    AFD = amp_mod_audio(FDIM,Chords[:,4],modamp)
+    
+    DIM = getCSM(Data, DIM)
+    ADM = amp_mod_audio(DIM,Chords[:,5],modamp)
+    
+    TRI = getCSM(Data, TRI)
+    ATT = amp_mod_audio(TRI,Chords[:,6],modamp)
+    
+    Audio = AMJS + AMJ + AMN + AMNS + AFD + ADM + ATT
+    return Audio    
+    
