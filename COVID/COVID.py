77eb819d239f759e90e517d0ad0aedca1237d98e
mtecce <60228508+mtecce@users.noreply.github.com>
2020-07-17 13:26:45 -0400

    Updated COVID,sounds work, variation is slight, has working graphs and cleaned up code with documentation

--- COVID/COVID.py
+++ COVID/COVID.py
@@ -18,22 +18,26 @@ from SlidingWindow import *
 from Viterbi import getCSM
 
 def get_data(countrycode):
-    
     """
-    country code = 2 char string for country
-    returns said countries data
+    Returns sect of COVID.csv containing a specified countries data
+    Parameters
+    ----------
+    countrycode:  string
+        Country Code used to identify the rows in COVID.csv
+    Returns
+    -------
+    data: ndarray()
+        All rows containing the specified country code
     """
     data = pd.read_csv('COVIDDATA.csv')
     data = data.to_numpy()
-    
     notC = True
     startindex = 0
     while notC:
         if data[startindex,1] == countrycode:
             notC = False
         else:
-            startindex += 1
-            
+            startindex += 1     
     isC = True
     endindex = startindex
     while isC:
@@ -41,16 +45,23 @@ def get_data(countrycode):
             endindex += 1
         else:
             isC = False
-    
     return data[startindex:endindex]
 
 def get_info(Data):
     
     """
-    data = matrix of data
-    returns the cases and deaths per day 
+    Return the Cases per Day & Deaths Per Day of the country's data matrix
+    Parameters
+    ----------
+    Data:  ndarray()
+        Data matrix of specific country to be spliced
+    Returns
+    -------
+    cases: array()
+        Array of Cases Per Day in chronological order
+    deaths:  array()
+        Array of Deaths Per Day in chronological order
     """
-    
     cases = (Data[:,4]).astype(int)
     cases = make_positive(cases)
     deaths = (Data[:,6]).astype(int)
@@ -60,26 +71,57 @@ def get_info(Data):
 def make_positive(Data):
     
     """
-    takes virus data matrix
-    returns the data made positive (to correct errors in the data)
+    Returns Data with all positive values (to correct errors in COVID data)
+    Parameters
+    ----------
+    Data:  array()
+        Array of values to be made positive
+    Returns
+    -------
+    Data:  array()
+        Array of values made positive
     """
-    
     for i in range(len(Data)):
         if Data[i] < 0:
             Data[i] *= -1
     return Data
 
 def get_interp(C,D,DAF):
-    
     """
-    interpolates the data to a new size
+    Returns set of interpolated cases and deaths arrays
+    Parameters
+    ----------
+    C:  array()
+        Array of cases to be interpolated
+    D:  array()
+        Array of deaths to be interpolated
+    DAF:  np.linspace()
+        Array of set size wished for interpolation of C & D
+    Returns
+    -------
+    IC:  array()
+        Interpolated cases array
+    ID:  array()
+        Interpolated deaths array
     """    
     IC = interp_data(C,DAF)
     ID = interp_data(D,DAF)
     return IC,ID
 
 def interp_data(Data, DAF):
-    #Interpolate Time Series to new size
+    """
+    Returns an interpolated array to size of DAF
+    Parameters
+    ----------
+    Data:  array()
+        Array to be interpolated
+    DAF:  np.linspace()
+        Array of set size used to calculate interpolation
+    Returns
+    -------
+    f(xnew):  array()
+        Interpolated Data array
+    """
     N = len(Data)
     x = np.linspace(0, 1, N)
     f = interpolate.interp1d(x,Data,kind='linear')
@@ -88,7 +130,17 @@ def interp_data(Data, DAF):
 
 def create_2D_shape(Cases,Deaths):
     """
-    makes a 2D array of cases and deaths in one matrix
+    Returns a 2D array copmrised of cases and deaths
+    Parameters
+    ----------
+    Cases:  array()
+        array containing cases per day
+    Deaths:  array()
+        array containing deaths per day
+    Returns
+    -------
+    Shape:  ndarray()
+        2D array, column 1 = cases, column 2 = deaths
     """
     s = len(Cases)
     Shape = np.zeros((s,2))
@@ -98,22 +150,49 @@ def create_2D_shape(Cases,Deaths):
  
 def get_country_max(X):
     """
-    gets largest value in matricies
+    Returns largest value in each column of a 2D matrix
+    Parameters
+    ----------
+    X:  ndarray()
+        Array of Data to be calculated
+    Returns
+    -------
+    cmax:  int
+        Max value in 1st column of X
+    dmax:  int
+        Max value in 2nd column of X
     """
     cmax = np.max(X[:,0])
     dmax = np.max(X[:,1])
     return cmax,dmax
 
-def scale_data(Data):
+def scale_data(Data,cscale,dscale,scaledown = True):
     """
-    scales data down to 0-1 scale
+    Returns scaled data on both columns
+    Parameters
+    ----------
+    Data:  ndarray()
+        Array of data to be scaled
+    scaledown: bool
+        determines if data is scaled down or up
+    cscale: float
+        how much to multiply column 1 by
+    dscale:  float
+        how much to multiply column 2 by
+    Returns
+    -------
+    Data:  ndarray()
+        Array of scaled data
     """
-    cmax,dmax = get_country_max(Data)
-    Data[:,0] /= cmax
-    Data[:,1] /= dmax
+    if scaledown == True:
+        Data[:,0] /= cscale
+        Data[:,1] /= dscale
+    else:
+        Data[:,0] *= cscale
+        Data[:,1] *= dscale
     return Data
     
-def create_earcons(cmax,dmax):
+def create_earcons(cmax,dmax,size):
     """
     MAJ8:
         a,d
@@ -134,26 +213,31 @@ def create_earcons(cmax,dmax):
     TRI7:
         c,f
     """
-    a = cmax/10000
-    b = cmax/2
-    c = cmax
-    d = dmax/10000
-    e = dmax/2
-    f = dmax
-    
-    MAJW = ([a,d])
-    MAJS = ([b,d])
-    MAJ = ([c,d])
-    MINS = ([a,e])
-    DIM = ([b,e])
-    AUG = ([c,e])
-    MIN = ([a,f])
-    FDIM = ([b,f])
-    TRI = ([c,f])
-    earcons = np.array([MAJW,MAJS,MAJ,MINS,DIM,AUG,MIN,FDIM,TRI])
-    return earcons
+    x = np.linspace(cmax/10000,cmax,size)
+    y = np.linspace(dmax/10000,dmax,size)
+    X,Y = np.meshgrid(x,y)
+    E = np.array([X.flatten(), Y.flatten()]).T
+    return E
 
-def make_chord_arrays(Seconds,Types,freq):    
+def make_chord_arrays(Seconds,Types,freq):
+    """
+    Returns array of audio arrays
+
+    Parameters
+    ----------
+    Seconds: int
+        Desired length of audio array to be created
+    Types: array(String)
+        Array of strings of the type of chord for a specific index
+    freq: int
+        Desired frequency for chords to be based from
+
+    Returns
+    -------
+    Chords : ndarray()
+        Array of audio arrays
+
+    """
     fs = 44100
     note = np.linspace(0,Seconds,fs*Seconds)
     numchords = int(len(Types))
@@ -186,14 +270,52 @@ def make_chord_arrays(Seconds,Types,freq):
             Chords[:,i] = sins[0] + sins[3] + sins[6] + sins[9]
     return Chords
 
-def amp_mod_audio(dists,AS,modamp):
-    #apply amplitude modulation (I used exponential because simple multiplication didn't have a usable effect)
+def amp_mod_audio(Dists,AS,modamp):
+    """
+    Returns audio array with amplitude modulation applied
+
+    Parameters
+    ----------
+    Dists:  array()
+        Array of distances used for amplitude modulation
+    AS: array()
+        Audio array that will have amplitude modulation applied
+    modamp: float
+        Value to help calculate amount of modulation at a specific distance
+
+    Returns
+    -------
+    Audio: array()
+        Audio array with amplitude modulation applied
+
+    """
     ampchange = 1.0 + modamp
-    Audio = AS[:] / (0.05 + dists[:]**ampchange)
-    #Audio = AS[:]*np.exp(-dists[:]**ampchange)
+    Audio = AS[:] / (0.01 + Dists[:]**ampchange)
+    #Audio = AS[:]*np.exp(-Dists[:]**ampchange)
     return Audio
 
 def create_audio(Data,Earcons,Chords,modamp):
+    """
+    Returns an audio array with amplitude modulation applied based by the distance
+    from a coordinate in Data to an Earcon coordinate
+
+    Parameters
+    ----------
+    Data : ndarray()
+        Cases/Deaths matrix used to calculate distance to earcon
+    Earcons : ndarray()
+        Array of coordinate points on a 2D grid to calculate distance for amplitude modulation
+    Chords : ndarray()
+        Array of audio arrays whose amplitude will be modulated based on distance to a specific earcon
+    modamp: float
+        Value to help calculate amount of modulation at a specific distance
+
+    Returns
+    -------
+    Audio : array()
+        Audio array comprised of all amplitude modulated chords
+
+    """
     Audio = np.zeros((len(Data)))
     Dists = getCSM(Data, Earcons)
     Dists = Dists/np.max(Dists)    
@@ -202,41 +324,104 @@ def create_audio(Data,Earcons,Chords,modamp):
         Audio += newaudio    
     return Audio
 
-if __name__ == '__main__':    
+def make_casesdeaths_plot(countrycode,Cases,Deaths):
+    plt.figure(figsize=(8,8))
+    plt.subplot(211)
+    plt.plot(Cases)
+    plt.ylabel("Cases Per Day")
+    plt.xlabel("Days")
+    plt.title("New Cases (" + countrycode + ")")
+    
+    plt.subplot(212)
+    plt.plot(Deaths)
+    plt.ylabel("Deaths Per Day")
+    plt.xlabel("Days")
+    plt.title("New Deaths (" + countrycode + ")")
+    
+def make_data_earcon_plot(Data,Earcons,countrycode,DesiredAudioFrame):
+    
+    maxc,maxd = get_country_max(Data)
+    plt.figure(figsize=(8,8))
+    plt.scatter(Data[:, 0], Data[:, 1], c = DesiredAudioFrame)
+    plt.scatter(Earcons[:, 0], Earcons[:, 1])
+    #plt.xlim(maxc)
+    #plt.ylim(maxd)
+    plt.xlabel("Cases Per Day")
+    plt.ylabel("Deaths Per Day")
+    plt.title("Cases/Deaths Earcons Graph of (" + countrycode + ")")
+
+def make_data_earcon_population_plot(Data,Earcons,countrycode,DesiredAudioFrame):
+    
+    maxc,maxd = get_country_max(Data)
+    plt.figure(figsize=(8,8))
+    plt.scatter(Data[:, 0], Data[:, 1], c = DesiredAudioFrame)
+    plt.scatter(Earcons[:, 0], Earcons[:, 1])
+    plt.xlim(0-(maxc*.2),maxc*1.2)
+    plt.ylim(0-(maxd*.2),maxd*1.2)
+    plt.xlabel("Cases Per Day")
+    plt.ylabel("Deaths Per Day")
+    plt.title("Cases/Deaths Earcons Graph of (" + countrycode + ")")
+
+def do_covid_calc(countrycode,DesiredAudioFrame,Chords,rcsize,popsize):
+    
+    #covid data from january 20th to july 10th 2020
+    Datamatrix = get_data(countrycode)
+    #get deaths and cases from each country
+    C,D = get_info(Datamatrix) 
+    #make cases deaths graph
+    make_casesdeaths_plot(countrycode,C,D)
+    
+    #interpolate the arrays to desired audio length
+    IC,ID = get_interp(C,D,DesiredAudioFrame)
+    
+    #create 2D arrays from cases/deaths 
+    Data = create_2D_shape(IC,ID)
+    
+    #get new max for cases and deaths
+    ncmax, ndmax = get_country_max(Data)
+    
+    #scales data down to [0,1] interval on both x,y axis
+    Data = scale_data(Data,ncmax,ndmax)
+    
+    #gets max values in each column of countries data matrix
+    cmax,dmax = get_country_max(Data)
+    
+    #create earcons
+    Earcons = create_earcons(cmax,dmax,rcsize)
+    
+    #create audio
+    modamp = 2
+    Audio = create_audio(Data,Earcons,Chords,modamp)
+    
+    #scale up  data/earcons for graph
+    GData = scale_data(Data,ncmax,ndmax,False)
+    GEarcons = scale_data(Earcons,ncmax,ndmax,False)
+    make_data_earcon_plot(GData,GEarcons,countrycode,DesiredAudioFrame)
+    
+    
+    #make population % graph earcon plot
+    PData = scale_data(GData,popsize,popsize)
+    PEarcons = scale_data(GEarcons,popsize,popsize)
+    make_data_earcon_population_plot(PData,PEarcons,countrycode,DesiredAudioFrame)
+    
+    return Audio
+
+if __name__ == '__main__': 
+    
     #covid data from january 20th to july 10th 2020
     UScode = "US"
-    ITcode = "IT"
-    USData = get_data(UScode)
-    ITData = get_data(ITcode)
-    
-    #get deaths and cases from each country
-    USC,USD = get_info(USData)
-    ITC,ITD = get_info(ITData)
-    
+    USPop = 329064917
+
     #interpolate 
     fs = 44100
     seconds = 1
     DesiredAudioFrame = np.linspace(0, seconds, int(fs*seconds))
-    IUSC,IUSD = get_interp(USC,USD,DesiredAudioFrame)
-    IITC,IITD = get_interp(ITC,ITD,DesiredAudioFrame)
-    
-    
-    USShape = create_2D_shape(IUSC,IUSD)
-    ITShape = create_2D_shape(IITC,IITD)
-    
-    USShape = scale_data(USShape)
-    ITShape = scale_data(ITShape)
-    
-    uscmax,usdmax = get_country_max(USShape)
-    itcmax,itdmax = get_country_max(ITShape)
-    
+
+    #creates chords to be used for amplitude modulation
     Types = ["MajorW","Major7","Major","Minor7","Dim","Aug","Minor","FullyDim","Tri"]
     freq = 440
     Chords = make_chord_arrays(seconds,Types,freq)
+    rcsize = int(np.sqrt(len(Types)))
     
-    USEarcons = create_earcons(uscmax,usdmax)
-    ITEarcons = create_earcons(itcmax,itdmax)
+    Audio = do_covid_calc(UScode,DesiredAudioFrame,Chords,rcsize,USPop)
     
-    modamp = .25
-    USAudio = create_audio(USShape,USEarcons,Chords,modamp)
-    ITAudio = create_audio(ITShape,ITEarcons,Chords,modamp)
